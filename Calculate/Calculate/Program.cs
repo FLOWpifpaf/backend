575084453ca808ccab8dd111ac96f8809881359b
FLOWpifpaf <79960811+FLOWpifpaf@users.noreply.github.com>
2022-03-17 14:51:00 +0300

    Добавил проект колькулятор

---
+++ Calculate/Calculate/Program.cs
@@ -0,0 +1,37 @@
+﻿string[] Operations = { "+", "-", "*", ":" };
+
+Console.WriteLine("Выберете операцию: \n - - Вычитание,\n + - Сложение,\n : - Деление,\n * - Умножение.");
+string? Symbol = Console.ReadLine();
+if (!Operations.Contains(Symbol))
+{
+    Console.WriteLine("Ошибка: не существует такой операции");
+    System.Environment.Exit(1);
+};
+
+Console.WriteLine("Введите первое число (тип double).");
+string? Num1 = Console.ReadLine();
+double Double1 = double.Parse(Num1);
+
+Console.WriteLine("Введите второе число (тип double).");
+string? Num2 = Console.ReadLine();
+double Double2 = double.Parse(Num2);
+
+double result = 0;
+if (Symbol == "-")
+{
+    result = Double1 - Double2;
+}
+if (Symbol == "+")
+{
+    result = Double1 + Double2;
+}
+if (Symbol == ":")
+{
+    result = Double1 / Double2;
+}
+if (Symbol == "*")
+{
+    result = Double1 * Double2;
+}
+
+Console.WriteLine("Результат: " + Double1 + Symbol + Double2 + "=" + result);
